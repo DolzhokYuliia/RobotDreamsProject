416d230723839c29779fd2f2f4792fb7607f53bd
Julia Dolzhok <juliadolzhok@MacBook-Air-Julia.local>
2023-01-17 20:05:01 +0100

    lesson 3

---
+++ src/com/ua/robot/yuliia/Lesson3.java
@@ -0,0 +1,12 @@
+package com.ua.robot.yuliia;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        int y = 4;
+                int r = 67;
+        System.out.println( y - r );
+        System.out.println( y + r );
+        System.out.println( r / y );
+        System.out.println( y * r );
+    }
+}
