a4493ba8da0b73c0cc2cce70a8ff38a1c28b4d52
juliadolzhok <jdolzhok@gmail.com>
2023-01-20 22:30:31 +0100

    changes

--- src/lesson4/Lesson4.java
+++ src/lesson4/Lesson4.java
@@ -9,8 +9,8 @@ public class Lesson4 {
         System.out.println(str.toLowerCase());
         int length = str.length();
         int lastIndex = length - 1;
+        int firstIndex = 0;
         System.out.println(str.charAt(lastIndex));
-
-
+        System.out.println(str.charAt(firstIndex));
     }
 }
