7a4a3d65e09938b591faa0e028c5c92114a0f218
Julia Dolzhok <juliadolzhok@MacBook-Air-Julia.local>
2023-01-20 22:22:32 +0100

    changes

--- src/lesson4/Lesson4.java
+++ src/lesson4/Lesson4.java
@@ -1,9 +1,15 @@
 package lesson4;
 
+import java.util.Locale;
+
 public class Lesson4 {
     public static void main(String[] args) {
-        String str="Hello, World!";
-        System.out.println(str);
+        String str ="Hello, World";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        int length = str.length();
+        int lastIndex = length - 1;
+        System.out.println(str.charAt(lastIndex));
 
 
     }
