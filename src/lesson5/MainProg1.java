244edcd6638e62b2853449f60052136a6125ec6c
juliadolzhok <jdolzhok@gmail.com>
2023-01-24 19:58:03 +0100

    changes

---
+++ src/lesson5/MainProg1.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class MainProg1 {
+    public static void main(String[] args) {
+        int number1 = 67;
+        int number2 = 45;
+                if (number1 > number2)
+                    System.out.println(number1>number2);
+    }
+}
+
+
---
+++ src/lesson5/MainProg2.java
@@ -0,0 +1,10 @@
+package lesson5;
+
+public class MainProg2 {
+    public static void main(String[] args) {
+        int number1 = 367;
+                int number2 = 567;
+                if (number1<number2)
+                    System.out.println(number1<number2);
+    }
+}
