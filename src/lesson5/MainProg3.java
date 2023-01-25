faaab610bd8679cc65bfc7d195b027d7ce81c876
juliadolzhok <jdolzhok@gmail.com>
2023-01-25 13:16:53 +0100

    changes

--- src/lesson5/MainProg3.java
+++
@@ -1,9 +0,0 @@
-package lesson5;
-
-public class MainProg3 {
-    public static void main(String[] args) {
-        int number = 4567;
-        if (number % 2 == 0)
-            System.out.println(number + "is even");
-    }
-}
