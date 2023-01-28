05a6b1cc28efa8da46679cd32f6cb2c01063fefd
juliadolzhok <jdolzhok@gmail.com>
2023-01-28 21:43:15 +0100

    changes

--- src/lesson6/Lesson6.java
+++ src/lesson6/Lesson6.java
@@ -1,4 +1,24 @@
 package lesson6;
-
+import java.util.Scanner;
 public class Lesson6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        int i = 1;
+        for (int a = 1; a <= 100; a++) {
+            if (a % 2 == 0)
+            System.out.print(a + " ");
+        }
+        System.out.println( );
+
+    }
 }
+
+
+
+
+
+
+
