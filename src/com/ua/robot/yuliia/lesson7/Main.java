0c69885e15c734b3b5fc8afe3c41439661db5292
juliadolzhok <jdolzhok@gmail.com>
2023-02-05 15:26:01 +0100

    changes

---
+++ src/com/ua/robot/yuliia/lesson7/Main.java
@@ -0,0 +1,17 @@
+package com.ua.robot.yuliia.lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        double number1 = 34;
+        double number2 = 56;
+        getCube(number1);
+        System.out.println(getCube(number1));
+
+    }
+
+    static double getCube(double cube) {
+        return Math.pow(cube, 3);
+    }
+    static
+}
+
