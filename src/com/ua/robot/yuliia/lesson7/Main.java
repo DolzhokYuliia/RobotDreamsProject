52250bb95408824a4d39872db8cd0933ceb72fba
juliadolzhok <jdolzhok@gmail.com>
2023-02-05 17:42:19 +0100

    changes

--- src/com/ua/robot/yuliia/lesson7/Main.java
+++ src/com/ua/robot/yuliia/lesson7/Main.java
@@ -3,15 +3,25 @@ package com.ua.robot.yuliia.lesson7;
 public class Main {
     public static void main(String[] args) {
         double number1 = 34;
-        double number2 = 56;
+        double number2 = 14;
         getCube(number1);
         System.out.println(getCube(number1));
+        int myNumber2 = (int)  number2;
+        printMaxStars(myNumber2);
 
     }
 
     static double getCube(double cube) {
         return Math.pow(cube, 3);
     }
-    static
-}
+
+    static void printMaxStars (int number){
+        for(int i = 0; i <= number; i++)
+            {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+
 
